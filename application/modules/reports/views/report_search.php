ddab8e1e38feaa67b5b95bac0c22f4bac1f45e81
mdarmancse <65160577+mdarmancse@users.noreply.github.com>
2021-11-08 11:10:25 +0600

    Complte -> Confirmd booking status

--- application/modules/reports/views/report_search.php
+++ application/modules/reports/views/report_search.php
@@ -18,7 +18,8 @@
                                 <option value="" selected="selected"><?php echo display('select_staus') ?>
                                 </option>
                                 <option value="0"><?php echo display('pending') ?></option>
-                                <option value="2"><?php echo display('complete') ?></option>
+                                <option value="3">Checked Out</option>
+                                <option value="2">Confirmed</option>
                                 <option value="1"><?php echo display('cancel') ?></option>
                             </select>
                         </div>
@@ -114,7 +115,7 @@
                                             <td><?php echo html_escape($book->checkindate);?></td>
                                             <td><?php echo html_escape($book->checkoutdate);?></td>
                                             <td><?php echo html_escape($book->date_time);?></td>
-                                            <td><?php if($book->bookingstatus==0){echo display('pending');}if($book->bookingstatus==1){echo display('cancel');}if($book->bookingstatus==2){echo display('complete');}?>
+                                            <td><?php if($book->bookingstatus==0){echo display('pending');}if($book->bookingstatus==1){echo display('cancel');}if($book->bookingstatus==2){echo 'Confirmed';}if($book->bookingstatus==3){echo 'Checked Out';}?>
                                             </td>
                                             <td><?php if($book->total_price>$book->paid_amount){echo display('pending');}else{echo display('complete');}?>
                                             </td>
